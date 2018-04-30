5bb0dbd02bf4962dc4e9148d370c0604eea817d8
viktor6 <viktor6@ukr.net>
2018-04-30 11:51:40 +0300

    up

--- times.py
+++ times.py
@@ -27,31 +27,31 @@ device = max7219(serial, cascaded=4, block_orientation=-90)
 
 
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28-1f653c126461')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
+def gettemp(id):
+  try:
+    mytemp = ''
+    filename = 'w1_slave'
+    f = open('/sys/bus/w1/devices/' + id + '/' + filename, 'r')
+    line = f.readline() # read 1st line
+    crc = line.rsplit(' ',1)
+    crc = crc[1].replace('\n', '')
+    if crc=='YES':
+      line = f.readline() # read 2nd line
+      mytemp = line.rsplit('t=',1)
+    else:
+      mytemp = 99999
     f.close()
-    return lines
-
-#CELSIUS CALCULATION
-def read_temp_c():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = int(temp_string) / 1000.0-1 # TEMP_STRING IS THE SENSOR OUTPUT, MAKE SURE IT'S AN INTEGER TO DO THE MATH
-        temp_c = str(round(temp_c, 1)) # ROUND THE RESULT TO 1 PLACE AFTER THE DECIMAL, THEN CONVERT IT TO A STRING
-        return temp_c
+ 
+    return int(mytemp[1])
+ 
+  except:
+    return 99999
+ 
+if __name__ == '__main__':
+ 
+  # Script has been called directly
+  id = '28-5ee327126461'
+  #print "Temp : " + '{:.3f}'.format(gettemp(id)/float(1000))
 
 #FAHRENHEIT CALCULATION
 sensor = BMP085.BMP085()
@@ -67,7 +67,7 @@ while(True):
 	
 	t = time.strftime("%H:%M")
 	d = time.strftime("%d/%m/%Y")
-	y = ("  Улица = " + read_temp_c() + " C")
+	y = ("  Улица = " + '{:.1f} C'.format(gettemp(id)/float(1000)))
 	k = ('  Комната = {0:0.1f} C'.format(sensor.read_temperature()))
 	#message = 'Temp: " + read_temp_c() + unichr(223) + "C'
 
